d9d21b11b6eda598314fd8103b39f36c4f0e7b1e
lebri <lebri88@gmail.com>
2019-12-12 16:37:01 +0300

    upd

--- st.c
+++ st.c
@@ -13,25 +13,12 @@ void	ft_summ_strings(char res[310], char temp[310])
 
 		i = 0;
 		ost = 0;
-/*		i = 310;
-		printf("res = ");
-		while (i > -1)	
-				printf("%c", res[i--]);
-		printf("\ntemp = ");
-		i = 310;
-		while (i > -1)
-				printf("%c", temp[i--]);
-		printf("\n");
-		i = 0;*/
 		while (temp[i] != 0)
 		{
 				if (res[i] == 0 && temp[i] != 0)
 						res[i] = '0';
 				tmp = (temp[i] - '0') + (res[i] - '0') + ost;
-				if (ost == 1)
-						ost = 0;
-				if (tmp / 10 > 0)
-						ost = 1;
+				ost = tmp / 10 > 0 ? 1 : 0;
 				res[i] = tmp % 10 + '0';
 				i++;
 				if (res[i] == 0 && ost == 1)
@@ -40,18 +27,17 @@ void	ft_summ_strings(char res[310], char temp[310])
 						ost = 0;
 				}
 		}
-		/*	while (res[i] != 0)
-			i++;
-			while (i >= 0)
-			printf("%c", res[--i]);*/
 }
+
+/*
+SPLIT ft_neg_pow on 2-3 ft somehow
+*/
 
 void	ft_neg_pow(char temp[310], int pow)
 {
 
 		int		i;
 		int		ost;
-		int		fut_ost;
 		int		tmp;
 		int		zero_ct;
 
@@ -60,44 +46,25 @@ void	ft_neg_pow(char temp[310], int pow)
 				temp[i--] = 0;//ft_bzero;
 		temp[i] = '1';
 		ost = 0;
-		fut_ost = 0;
 		zero_ct = 0;
-//		write(1, "HERE", 4);
 		while (pow > 0)
 		{
 				while (temp[i] != 0)
 				{
-						if (fut_ost != 0)
-						{
-								ost = fut_ost;
-								fut_ost = 0;
-						}
-						tmp = temp[i] - '0';
-						tmp = tmp * 5 + ost;
-						if (tmp / 10 > 0)
-								fut_ost = tmp / 10;
+						tmp = (temp[i] - '0') * 5 + ost;
+						ost = tmp / 10 > 0 ? (tmp / 10) : 0;
 						temp[i] = tmp % 10 + '0';
-						ost = 0;
 						i++;
 				}
-				if (temp[i] == 0 && fut_ost != 0)
+				if (temp[i] == 0 && ost > 0)
 				{
-						temp[i] = fut_ost + '0';
-						fut_ost = 0;
+					temp[i] = ost + '0';
+					ost = 0;
 				}
-//				printf("temp[i] = %c, i = %i\n", temp[i], i);
 				if (temp[i] == '1')
-				{
-//						printf("@@@@@@@");
 						zero_ct++;
-				}
-				pow--;
-//				i = 0;
-//				printf("\nnbr = ");
-//				while (temp[i] != 0)
-//						printf("%c", temp[i++]);
-//				printf("\n");
 				i = 0;
+				pow--;
 		}
 		i = 0;
 		while (temp[i] != 0)
@@ -110,12 +77,6 @@ void	ft_neg_pow(char temp[310], int pow)
 		}
 		temp[i] = '.';
 		temp[i + 1] = '0';
-		
-/*		i = 0;
-		printf("zero count = %i\n", zero_ct);
-		while (i < 15)
-				printf("%c", temp[i++]);
-				*/ 
 }
 /*
 EACH TIME FIRST NUMBER OF SQUARE 5 IS 1 (CANNOT BE DELETED TO 2 W/O PROBLEMS) 0.0 is added to final nbr!
@@ -159,7 +120,7 @@ int	main(int argn, char **argv)
 		while (j >= 0)
 				printf("%c", res[j--]);
 		printf("\n");
-		ft_neg_pow(temp, 10);			/*ft_negative_*/
+		ft_neg_pow(temp, 15);			/*ft_negative_*/
 		j = 0;
 		while (temp[j] != 0)
 				j++;
@@ -174,43 +135,28 @@ void ft_power_to_two(char temp[310], int pow)
 {
 		int		i;
 		int		ost;
-		int		fut_ost;
 		int		tmp;
 
 		i = 309;
 		while (i > 0)
-				temp[i--] = 0;//ft_bzero;
+				temp[i--] = 0;
 		temp[i] = '1';
 		ost = 0;
-		fut_ost = 0;
 		while (pow > 0)
 		{
 				while (temp[i] != 0)
 				{
-						if (fut_ost == 1)
-						{
-								ost = 1;
-								fut_ost = 0;
-						}
-						tmp = temp[i] - '0';
-						tmp = tmp * 2 + ost;
-						if (tmp / 10 > 0)
-								fut_ost = 1;
+						tmp = (temp[i] - '0') * 2 + ost;
+						ost = tmp / 10 > 0 ? 1 : 0;
 						temp[i] = tmp % 10 + '0';
-						ost = 0;
 						i++;
 				}
-				if (temp[i] == 0 && fut_ost == 1)
+				if (temp[i] == 0 && ost == 1)
 				{
-						temp[i] = '1';
-						fut_ost = 0;
+					temp[i] = '1';
+					ost = 0;
 				}
-				pow--;
 				i = 0;
+				pow--;
 		}
-		/*	while (temp[i] != 0)
-			i++;
-			while (i >= 0)
-			printf("%c", temp[--i]);
-			printf("\n");*/
 }
